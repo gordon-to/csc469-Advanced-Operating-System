6ed6509701747be277a769621d682d8e296e2bee
yuch7 <yuchen7990@gmail.com>
2016-02-03 12:15:21 -0500

    added some stuff

--- parta.c
+++ parta.c
@@ -1,18 +1,50 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include "tsc.h"
 #include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sched.h>
+
+extern uint64_t start;
 
 
 uint64_t inactive_periods(int num, uint64_t threshold, uint64_t *samples){
 
+	start_counter();
+	uint64_t ret = get_counter(), tmp;
+	for (int i = 0; i < num; i++){
+		start_counter();
+		tmp = get_counter();
+		printf("Active %d: start at %u, duration %u cycles\n", i, start, tmp);
+	}
+	return ret;
 }
 
 
-int main () {
+int main (int argc, char ** argv) {
 
-	start_counter();
-	uint64_t tmp = get_counter();
-	printf("%u\n", tmp);	
+	int num, length;
+	cpu_set_t cpuset;
+	uint64_t threshold;
+	uint64_t *samples;
+	length = num;
+	samples = malloc(sizeof(uint64_t) * length);
+
+	if (argc > 2) {
+		fprintf(stderr, "%s\n", "Usage parta (optional) <num>");
+		exit(0);
+	}
+
+	num = (argc == 2) ? atoi(argv[1]) : 1;
+
+	CPU_SET(0, &cpuset);
+
+	if (sched_setaffinity(getpid(), sizeof(cpuset), &cpuset) == -1)
+		fprintf(stderr, "%s\n", "sched_setaffinity");
+
+	inactive_periods(num, threshold, samples);
+
 	return 0;
 	
 }
--- tsc.h
+++ tsc.h
@@ -2,6 +2,7 @@
 
 #ifndef TSC_H
 #define TSC_H
+uint64_t start;
 extern void start_counter();
 extern uint64_t get_counter();
 #endif

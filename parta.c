4ce892ff24080362fd0414d77860387b54ee9a06
yuch7 <yuchen7990@gmail.com>
2016-02-07 15:18:59 -0500

    cpu freq partially working

--- parta.c
+++ parta.c
@@ -11,14 +11,23 @@ extern uint64_t start;
 
 uint64_t inactive_periods(int num, uint64_t threshold, uint64_t *samples){
 
-	start_counter();
-	uint64_t ret = get_counter(), tmp;
+	uint64_t ret = get_counter(), tmp = ret;
 	for (int i = 0; i < num; i++){
 		start_counter();
-		tmp = get_counter();
+		while(get_counter() < threshold)
+			
 		printf("Active %d: start at %u, duration %u cycles\n", i, start, tmp);
+		tmp = get_counter();
 	}
 	return ret;
+}
+
+void getcpu_freq(uint64_t * cpufreq){
+
+	int microseconds = 1;
+	start_counter();
+	usleep(microseconds);
+	*cpufreq = get_counter() * (100/microseconds);
 }
 
 
@@ -28,7 +37,9 @@ int main (int argc, char ** argv) {
 	cpu_set_t cpuset;
 	uint64_t threshold;
 	uint64_t *samples;
+	uint64_t *cpufreq;
 	length = num;
+	cpufreq = malloc(sizeof(uint64_t));
 	samples = malloc(sizeof(uint64_t) * length);
 
 	if (argc > 2) {
@@ -38,10 +49,17 @@ int main (int argc, char ** argv) {
 
 	num = (argc == 2) ? atoi(argv[1]) : 1;
 
+	CPU_ZERO(&cpuset);
 	CPU_SET(0, &cpuset);
 
 	if (sched_setaffinity(getpid(), sizeof(cpuset), &cpuset) == -1)
 		fprintf(stderr, "%s\n", "sched_setaffinity");
+
+	//getcpu frequency
+	while(1){
+		getcpu_freq(cpufreq);
+		printf("%u\n", *cpufreq);
+	}
 
 	inactive_periods(num, threshold, samples);
 
